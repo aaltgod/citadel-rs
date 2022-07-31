06f6a22df23009a962336258c81cdf1ee85ca2d2
aaltgod <minealaska29@gmail.com>
2022-07-31 19:10:22 +0300

    add actix-web

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, HttpResponse, HttpServer, Responder, App};
+
+
+#[get("/")]
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("Hi there")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(home)
+    })
+    .bind(("localhost", 8080))?
+    .run()
+    .await
 }
