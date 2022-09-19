08b89975e73affc681832460db65a594b5399772
aaltgod <minealaska29@gmail.com>
2022-09-19 23:05:35 +0300

    add flexbox for homepage

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -6,12 +6,17 @@ pub fn view() -> Html {
     let box_style = css!(
         r#"
             display: flex;
+            flex-direction: row;
             justify-content: flex-start;
             padding: 50px;
             margin: 20px;
+            align-items: center;
+            flex-flow: row wrap;
+            @media (max-width: 900px) {
+                flex-direction: column;
+            }
         "#
     );
-
     let item_style = css!(
         r#"
             flex: 1;
@@ -23,10 +28,15 @@ pub fn view() -> Html {
             margin: 10px;
             width: 200px;
             height: 250px;
+            cursor: pointer;
+            text-align: center;
+            text-transform: uppercase;
+            color: white;
         "#
     );
 
-    let items: [&str; 2] = ["archive1", "archive2"];
+
+    let items: [&str; 10] = ["archive1", "archive2", "archive1", "archive2", "archive1", "archive2", "archive1", "archive2", "archive1", "archive2"];
 
     html! {
         <>
@@ -35,9 +45,11 @@ pub fn view() -> Html {
                 {
                     items.into_iter().map(|item| {
                         html! {
-                            <div class={item_style.clone()}> {item}
-                                <a href="#"><span /> </a>
-                            </div>
+                            <a href="#">
+                                <div class={item_style.clone()}> {item}
+                                </div>
+                            </a>
+
                         }   
                     })
                     .collect::<Html>()
